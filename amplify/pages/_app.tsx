2c64ce68f5c173028d7c697ece9a047ed7291a42
larryculver1226 <larry.culver1226@gmail.com>
2024-10-10 18:26:16 +0000

    Added Amplify Authentication app

---
+++ amplify/pages/_app.tsx
@@ -0,0 +1,21 @@
+import type { AppProps } from 'next/app';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import outputs from '@/amplify_outputs.json';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <Component {...pageProps} />
+        </main>
+      )}
+    </Authenticator>
+  );
+};
